db049bd64bd2f30bdcd4d9fc98f1b51b3d896bf1
Володимир <143431245+Qwer-Ghost@users.noreply.github.com>
2024-12-14 18:50:31 +0200

    Add files via upload

---
+++ script II .js
@@ -0,0 +1,40 @@
+function copyText_A() {
+    var text = document.getElementById("textToCopy_A").innerText;
+    navigator.clipboard.writeText(text).then(function() { alert("Текст скопійовано."); }, 
+    function(err) { alert("Не вдалося скопіювати текст: ", err); });
+}
+function copyText_B() {
+    var text = document.getElementById("textToCopy_B").innerText;
+    navigator.clipboard.writeText(text).then(function() { alert("Текст скопійовано."); }, 
+    function(err) { alert("Не вдалося скопіювати текст: ", err); });
+}
+function copyText_C() {
+    var text = document.getElementById("textToCopy_C").innerText;
+    navigator.clipboard.writeText(text).then(function() { alert("Текст скопійовано."); }, 
+    function(err) { alert("Не вдалося скопіювати текст: ", err); });
+}
+function copyText_D() {
+    var text = document.getElementById("textToCopy_D").innerText;
+    navigator.clipboard.writeText(text).then(function() { alert("Текст скопійовано."); }, 
+    function(err) { alert("Не вдалося скопіювати текст: ", err); });
+}
+function copyText_E() {
+    var text = document.getElementById("textToCopy_E").innerText;
+    navigator.clipboard.writeText(text).then(function() { alert("Текст скопійовано."); }, 
+    function(err) { alert("Не вдалося скопіювати текст: ", err); });
+}
+function copyText_F() {
+    var text = document.getElementById("textToCopy_F").innerText;
+    navigator.clipboard.writeText(text).then(function() { alert("Текст скопійовано."); }, 
+    function(err) { alert("Не вдалося скопіювати текст: ", err); });
+}
+function copyText_G() {
+    var text = document.getElementById("textToCopy_G").innerText;
+    navigator.clipboard.writeText(text).then(function() { lert("Текст скопійовано."); },
+    function(err) { alert("Не вдалося скопіювати текст: ", err); });
+}
+function copyText_H() {
+    var text = document.getElementById("textToCopy_H").innerText;
+    navigator.clipboard.writeText(text).then(function() { alert("Текст скопійовано."); }, 
+    function(err) { alert("Не вдалося скопіювати текст: ", err); });
+}
