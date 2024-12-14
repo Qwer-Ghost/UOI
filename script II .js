78e440af45ba048dbbae3d75940fa31df5e148a6
Володимир <143431245+Qwer-Ghost@users.noreply.github.com>
2024-12-14 19:21:42 +0200

    Update script II .js

--- script II .js
+++ script II .js
@@ -1,40 +1,40 @@
 function copyText_A() {
     var text = document.getElementById("textToCopy_A").innerText;
-    navigator.clipboard.writeText(text).then(function() { alert("Текст скопійовано."); }, 
+    navigator.clipboard.writeText(text).then(function() { alert("Код задачі A скопійовано."); }, 
     function(err) { alert("Не вдалося скопіювати текст: ", err); });
 }
 function copyText_B() {
     var text = document.getElementById("textToCopy_B").innerText;
-    navigator.clipboard.writeText(text).then(function() { alert("Текст скопійовано."); }, 
+    navigator.clipboard.writeText(text).then(function() { alert("Код задачі B скопійовано."); }, 
     function(err) { alert("Не вдалося скопіювати текст: ", err); });
 }
 function copyText_C() {
     var text = document.getElementById("textToCopy_C").innerText;
-    navigator.clipboard.writeText(text).then(function() { alert("Текст скопійовано."); }, 
+    navigator.clipboard.writeText(text).then(function() { alert("Код задачі C скопійовано."); }, 
     function(err) { alert("Не вдалося скопіювати текст: ", err); });
 }
 function copyText_D() {
     var text = document.getElementById("textToCopy_D").innerText;
-    navigator.clipboard.writeText(text).then(function() { alert("Текст скопійовано."); }, 
+    navigator.clipboard.writeText(text).then(function() { alert("Код задачі D скопійовано."); }, 
     function(err) { alert("Не вдалося скопіювати текст: ", err); });
 }
 function copyText_E() {
     var text = document.getElementById("textToCopy_E").innerText;
-    navigator.clipboard.writeText(text).then(function() { alert("Текст скопійовано."); }, 
+    navigator.clipboard.writeText(text).then(function() { alert("Код задачі E скопійовано."); }, 
     function(err) { alert("Не вдалося скопіювати текст: ", err); });
 }
 function copyText_F() {
     var text = document.getElementById("textToCopy_F").innerText;
-    navigator.clipboard.writeText(text).then(function() { alert("Текст скопійовано."); }, 
+    navigator.clipboard.writeText(text).then(function() { alert("Код задачі F скопійовано."); }, 
     function(err) { alert("Не вдалося скопіювати текст: ", err); });
 }
 function copyText_G() {
     var text = document.getElementById("textToCopy_G").innerText;
-    navigator.clipboard.writeText(text).then(function() { lert("Текст скопійовано."); },
+    navigator.clipboard.writeText(text).then(function() { lert("Код задачі G скопійовано."); },
     function(err) { alert("Не вдалося скопіювати текст: ", err); });
 }
 function copyText_H() {
     var text = document.getElementById("textToCopy_H").innerText;
-    navigator.clipboard.writeText(text).then(function() { alert("Текст скопійовано."); }, 
+    navigator.clipboard.writeText(text).then(function() { alert("Код задачі H скопійовано."); }, 
     function(err) { alert("Не вдалося скопіювати текст: ", err); });
 }
